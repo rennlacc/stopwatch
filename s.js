574f15ebce78979732b817e43ab9e45489f4cf8e
rennlacc <66423004+rennlacc@users.noreply.github.com>
2021-10-12 18:53:12 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,64 @@
+let hrs = 0;
+let min = 0;
+let sec = 0;
+let interval;
+let isStop = true;
+
+let displayHrs = 0;
+let displayMin = 0;
+let displaySec = 0;
+
+
+function stopwatch() {
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+            }
+        }
+        if(sec < 10){
+            displaySec = "0" + sec.toString();
+        }else{
+            displaySec = sec;
+        }
+       if(min < 10){
+                displayMin = "0" + min.toString();    
+        }else{
+            displayMin = min;
+        }
+        if(hrs < 10){
+            displayHrs = "0" + hrs.toString();    
+        }else{
+            displayHrs = hrs;
+        }
+    document.getElementById("sw").innerHTML = displayHrs + ":" + displayMin + ":" + displaySec;
+}
+
+
+function start() {
+    
+    if(isStop == true ) {
+        isStop = false
+        interval= setInterval(stopwatch,1000);
+    }
+}
+
+function stop() {
+    if(isStop == false) {
+      isStop = true; 
+      clearInterval(interval);
+    }
+}
+
+function reset() {
+    sec = 0;
+    min = 0;
+    hrs = 0;
+    document.getElementById("sw").innerHTML = "00:00:00";
+}
+
+
+
